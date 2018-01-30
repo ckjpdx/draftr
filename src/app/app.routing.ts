93a7ef894722e45357a391e8372c63d24093b54b
cworf <colin.worf@gmail.com>
2018-01-29 22:55:45 -0800

    add router

---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { AllProjectsComponent } from './all-projects/all-projects.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { LoginComponent } from './login/login.component';
+// import { AuthGuard } from './core/auth.guard';
+
+
+const appRoutes: Routes = [
+	{
+  		path: '',
+  		component: LoginComponent
+	},
+	{
+  		path: 'projects',
+  		component: AllProjectsComponent,
+		// canActivate: [AuthGuard]
+	},
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
