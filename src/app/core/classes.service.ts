e225c56e1cc6d5b9bfae81cf9e523513029d2496
cworf <colin.worf@gmail.com>
2018-01-30 20:38:00 -0800

    save class list into service

--- src/app/core/classes.service.ts
+++ src/app/core/classes.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ClassesService {
-
-  constructor() { }
+    classList: string[];
+  constructor() {
+      this.classList = [
+          "Intro to Programming",
+          "CSS",
+          "C#",
+          "Java",
+          "PHP",
+          "Ruby",
+          "JavaScript",
+          "Android",
+          "Design",
+          "Drupal",
+          ".NET",
+          "Rails",
+          "React",
+          "Ember.js"
+      ]
+  }
 
 }
