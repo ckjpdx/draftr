04db0a6c7744a1573529e4f48ac9da7f7b775c05
Guest User <Guest@Epicodus-3A.local>
2018-01-30 16:29:45 -0800

    add preliminary edit project function

--- src/app/edit-project/edit-project.component.ts
+++ src/app/edit-project/edit-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-edit-project',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProjectComponent implements OnInit {
 
+  @Input() selectedProject;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  doneEditing(){
+    
+  }
 }
