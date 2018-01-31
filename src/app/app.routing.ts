8204f549d3494999edb0fca61201df1fa46ca2ad
Chris K Johnson <cknightjohnson@gmail.com>
2018-01-31 09:57:54 -0800

    add auth guard to auth.service & routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { AllProjectsComponent } from './all-projects/all-projects.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { LoginComponent } from './login/login.component';
-// import { AuthGuard } from './core/auth.guard';
+import { AuthGuard } from './core/authguard.service';
 
 
 const appRoutes: Routes = [
@@ -15,17 +15,17 @@ const appRoutes: Routes = [
 	{
   		path: 'projects',
   		component: AllProjectsComponent,
-		// canActivate: [AuthGuard]
+			canActivate: [AuthGuard]
 	},
 	{
   		path: 'profile',
   		component: ProfileComponent,
-		// canActivate: [AuthGuard]
+			canActivate: [AuthGuard]
 	},
 	{
   		path: 'project-detail/:id',
   		component: ProjectDetailComponent,
-		// canActivate: [AuthGuard]
+			canActivate: [AuthGuard]
 	}
 
 ];
